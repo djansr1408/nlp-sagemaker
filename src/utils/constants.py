12575ea3713ff0e098b69ad947f9b75cc3c7e460
Srdjan Jovanovic <srdjan.jovanovic@synechron.com>
2021-12-13 21:13:11 +0100

    changed constants.py

--- src/utils/constants.py
+++ src/utils/constants.py
@@ -9,6 +9,8 @@ EOS_WORD = "<eos>"
 PAD_WORD = "<pad>"
 
 BASE_DIR = os.getenv("BASE_DIR")
-DATASET_DIR = os.getenv("DATASET_DIR")
-STORAGE_DIR = os.path.join(BASE_DIR, "storage")
+
+BASE_DIR = os.path.dirname(os.environ["SM_MODEL_DIR"])
+# DATASET_DIR = os.getenv("DATASET_DIR")
+# STORAGE_DIR = os.path.join(BASE_DIR, "storage")
 CONFIG_PATH = os.path.join(BASE_DIR, "config.json")
